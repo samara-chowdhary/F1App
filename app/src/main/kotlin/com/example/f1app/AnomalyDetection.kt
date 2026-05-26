eb51f13ff41936d3b0fc0ff37fecc4af46a75963
Samara Chowdhary <samarachowdhary@Mac.Home>
2026-05-26 14:39:04 +0100

    message

---
+++ app/src/main/kotlin/com/example/f1app/AnomalyDetection.kt
@@ -0,0 +1,68 @@
+package com.example.f1app
+
+import java.lang.Boolean.FALSE
+import java.lang.Boolean.TRUE
+import kotlin.math.sqrt
+
+//the function for training model
+val trainingData: List<List<Double>> =  listOf()
+val m  = 0
+val n = 0
+
+fun trainModel(trainingData: List<List<Double>>, m: Int, n: Int): Pair<DoubleArray, DoubleArray> {
+    val means = DoubleArray(n)
+    val variances = DoubleArray(n)
+
+    for(j in 0 until n -1){
+        //calculate the mean for feature j
+        var total = 0.0
+        for(i in 0 until m - 1){
+            total  = total + trainingData[i][j]
+        }
+
+        means[j] = total / m
+
+        //calculate variance for feature j
+        var sumSquaredDiff = 0.0;
+        for(i in 0 until m - 1){
+            val diff = trainingData[i][j] - means[j]
+            sumSquaredDiff = sumSquaredDiff + (diff * diff)
+        }
+        variances[j] = sumSquaredDiff / m
+    }
+
+    return Pair(means, variances)
+}
+
+//anomally detection function
+
+val xTest = DoubleArray(n)
+val means = DoubleArray(n)
+val variances = DoubleArray(n)
+val epsilon = 0.0
+
+fun isAnomalous(xTest: DoubleArray, means: DoubleArray, variances: DoubleArray, n: Int, epsilon: Double): Boolean {
+    var pTotal = 1.0
+    val constantPI = 3.14159
+    val e = 2.71828
+
+    for(j in 0 until n - 1){
+        val mu = means[j]
+        val sigmaSq = variances[j]
+        val x = xTest[j]
+
+        //implementing the formula
+        val exponent = -(Math.pow((x-mu), 2.0)) / (2 * sigmaSq)
+        val prob_j = (1 / sqrt(2.0 * constantPI * sigmaSq)) * (Math.pow(e, exponent))
+
+        //multiply probabilities
+        pTotal = pTotal * prob_j
+    }
+
+    if(pTotal < epsilon){
+        return TRUE //it is an anomaly
+    }
+    else{
+        return FALSE //it is normal
+    }
+}
---
+++ app/src/main/kotlin/com/example/f1app/MultiClassClassification.kt
@@ -0,0 +1,47 @@
+package com.example.f1app
+
+import kotlin.math.ln
+
+val multiWeights: List<List<Double>> = listOf()
+val biases = listOf<Double>()
+
+
+//function for calculating all of the z values
+fun calculateAllZ(multiWeights: List<List<Double>>, features: List<Double>, biases: List<Double>) {
+    val scores = DoubleArray(biases.size)
+
+    for (i in 0 until biases.size - 1) {
+        var z: Double = 0.0
+        for (j in 0 until features.size - 1) {
+            z = z + (multiWeights[i][j] * features[j])
+        }
+        scores[i] = z + biases[i]
+    }
+}
+
+//application of the softmax function
+fun applySoftmax(scores: DoubleArray): DoubleArray {
+    val probabilities = DoubleArray(scores.size)
+    var sumExp: Double = 0.0
+    val e = 2.71828
+
+    //calculate the denominator
+    for(i in 0 until scores.size - 1){
+        sumExp = sumExp + (Math.pow(e, scores[i]))
+    }
+
+    //divide each e^z by the sum
+    for(i in 0 until scores.size - 1){
+        probabilities[i] = Math.pow(e, scores[i]) / sumExp
+    }
+
+    return probabilities
+}
+
+//calculate the cross-entropy loss
+val trueClassIndex = 0
+
+fun calculateCrossEntropy(probabilities: DoubleArray, trueClassIndex: Int): Double {
+    val loss = -1 * ln(probabilities[trueClassIndex] + 0.000000001)
+    return loss
+}
